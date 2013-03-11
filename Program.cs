9135e755e3ece3000b928cf05076f19499cf3cd8
unknown <Zaj87@bk.ru>
2013-03-11 23:00:59 +0400

    11 March 2013

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Security;
+
+
+namespace RunAs
+{
+
+public class Connect
+    {
+        string login;
+        string password;
+        string domain;
+
+        public Connect(string login, string password, string domain)
+        {
+            this.login = login;
+            this.password = password;
+            this.domain = domain;
+        }
+
+
+        public void RunAs(string program)
+        {
+            try
+            {
+                ProcessStartInfo myProcess = new ProcessStartInfo(program);
+                myProcess.UserName = login;
+                //myProcess.Password = MakeSecureString(password); BIG Oops
+                myProcess.Domain = Environment.MachineName; //domain
+                myProcess.UseShellExecute = false;
+                myProcess.Verb = "runas";
+                Process.Start(myProcess);
+            }
+            catch
+            {
+                Console.WriteLine("Oops");
+            }
+        }
+
+    }
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Connect con = new Connect("User", "Password", "WORKGROUP");
+            con.RunAs("notepad");
+
+
+        }
+    }
+}
+
